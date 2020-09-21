ba5a7ba28fbf297354e3285490d86ed35dfc3156
QuadCrew4 <quadcrewz@gmail.com>
2020-09-21 15:20:15 +0530

    Claim Insurance

--- src/app/furtherdetails/furtherdetails.component.ts
+++ src/app/furtherdetails/furtherdetails.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 import { Router } from '@angular/router';
 import { BankDetailsModule } from '../BankDetails.model';
+import { CustomValidationService } from '../services/custom-validation.service';
 import { InsuranceService } from '../services/insurance.service';
 
 @Component({
@@ -13,58 +14,20 @@ import { InsuranceService } from '../services/insurance.service';
 export class FurtherdetailsComponent implements OnInit {
   bank=new BankDetailsModule;
   bankDetailsForm:FormGroup;
-  error_messages = {
-    'claimamount': [
-      { type: 'required', message: 'Claim Amount is required.' },
-    ],
-
-    'IFSCcode': [
-      { type: 'required', message: 'IFSC Code is required.' }
-    ],
-
-    'Holder Name': [
-      { type: 'required', message: 'Holder Name is required.' }
-      
-    ],
-
-    'accountno': [
-      { type: 'required', message: 'Account Number is required.' },
-      { type: 'minlength', message: 'Account Number length' },
-      { type: 'maxlength', message: 'Invalid Account Number' }
-    ],
-    'confirmaccountno': [
-      { type: 'required', message: 'Confirm Account Number is required.' },
-      { type: 'minlength', message: 'Invalid' },
-      { type: 'maxlength', message: 'Invalid' }
-    ],
-  }
+  
+  
 
 
-  constructor(private service:InsuranceService ,private router:Router, public formBuilder: FormBuilder){
+  constructor(private service:InsuranceService ,private router:Router, public formBuilder: FormBuilder,private customValidator:CustomValidationService){
    
     this.bankDetailsForm = this.formBuilder.group({
-      claimamount: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      IFSCcode: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      holdername: new FormControl('', Validators.compose([
-        Validators.required
-       
-      ])),
-      accountno: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(30)
-      ])),
-      confirmaccountno: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(30)
-      ])),
+      claimamount:["",[Validators.required,Validators.pattern("[0-9]{3,}")]],
+      IFSCcode: ["",[Validators.required,Validators.pattern("[A-Z0-9]{6,}")]],
+      holdername:["", [Validators.required,Validators.pattern("[A-Za-z]{3,}")]],
+      accountno: ["",[Validators.required,Validators.pattern("[0-9]{6,}")]],
+      confirmaccountno: ["",[Validators.required]]
     }, { 
-      validators: this.accountno.bind(this)
+      validator: this.customValidator.accountnoMatchValidator("accountno","confirmaccountno")
     });
   }
 
@@ -79,7 +42,7 @@ export class FurtherdetailsComponent implements OnInit {
     this.bank=new BankDetailsModule();
     alert(`
            Claim No:
-           Date: {{currentDate |date :'yyyy-MM-dd'}}
+           Date: 
            Policy No:
            Amount: 
           
@@ -89,13 +52,7 @@ export class FurtherdetailsComponent implements OnInit {
     
 
   }
-  success:String="Account Number Matched";
-  unsuccess:String="Account Number Mismatch";
-  accountno(formGroup: FormGroup) {
-    const { value: accountno } = formGroup.get('accountno');
-    const { value: confirmaccountno } = formGroup.get('confirmaccountno');
-    return accountno === confirmaccountno ? "": this.unsuccess;
-  }
+
   
 
 }
---
+++ src/app/services/custom-validation.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomValidationService {
+
+  constructor() { }
+  accountnoMatchValidator(accountno: string, confirmaccountno: string) {
+    return (bankDetailsForm: FormGroup) => {
+      const accountnoControl = bankDetailsForm.controls[accountno];
+      const confirmaccountnoControl = bankDetailsForm.controls[confirmaccountno];
+
+      if (!accountnoControl || !confirmaccountnoControl) {
+        return null;
+      }
+
+      if (
+        confirmaccountnoControl.errors &&
+        !confirmaccountnoControl.errors.accountnoMismatch
+      ) {
+        return null;
+      }
+
+      if (accountnoControl.value !== confirmaccountnoControl.value) {
+        confirmaccountnoControl
+        .setErrors({ accountnoMismatch: true });
+      } else {
+        confirmaccountnoControl.setErrors(null);
+      }
+    };
+  }
+
+}
