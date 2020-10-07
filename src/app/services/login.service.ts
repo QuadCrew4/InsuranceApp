461ce06083b2fcbb7faeeae9f48a51772eb781c5
QuadCrew4 <quadcrewz@gmail.com>
2020-10-07 17:22:33 +0530

    reset password service added

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
   import { HttpClient, HttpParams } from '@angular/common/http';
   import { Injectable } from '@angular/core';
   import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
   import { Login } from '../login.model';
   import { User } from '../user.model';
 
@@ -17,6 +18,10 @@ export class LoginService {
       return this.http.get<User[]>(this.baseUrl+"/list");
     }
 
+    findUser(username : string): Observable<User>{
+      return this.http.get<User>(this.baseUrl+"/fetch/"+username);;
+    }
+
     registerUser(user: User)
     {
       this.http.post(this.baseUrl+"/register",user).subscribe(data => data = user);
@@ -28,5 +33,8 @@ export class LoginService {
       let result = this.http.get<User>(this.baseUrl+"/login",{params : params}).toPromise();
       return result;
 
+    }
+    reset(uname : string, password: string, user : User){
+      return this.http.put(this.baseUrl+"/resetpassword/"+uname+","+password,null).subscribe(data => data = user);
     }
   }
